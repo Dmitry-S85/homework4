facdce45645fc3422cb0b6533f0a88aef8de91e4
Dmitry-S85 <dmitry.stoyankin@yandex.ru>
2024-06-22 17:13:12 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+#переменная my_string со строкой с произвольным текстом (функция input()).
+my_string = input()
+print(my_string)
+
+#Вывод длины символов введённого текста
+print(len(my_string))
+
+
